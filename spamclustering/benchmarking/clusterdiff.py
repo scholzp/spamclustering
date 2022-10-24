9b1a0c2ad31adcfe852de2ff08b7f1e53eec1f03
Pascal Scholz <pascal.scholz@tu-dresden.de>
2022-10-24 09:22:36 +0200

    ADD: New benchmarking script.
    Contains a script which can be used to queue multiple algorithms for
    benchmarking. These benchmarks will be run in parallel. A modified
    version of difftool was neccessarry for this task. Both version might
    be merged in the future.

---
+++ spamclustering/benchmarking/clusterdiff.py
@@ -0,0 +1,125 @@
+import pandas
+import os
+
+from sklearn import metrics
+
+
+def generate_cluster_diff(first, second):
+    series_dict = {}
+    # Create dicts which match cluster ids against cluster for faster
+    # search
+    self_clusters = {c.uuid: c for c in first}
+    other_clusters = {c.uuid: c for c in second}
+    # generate two sets, each containing all files for each cluster set
+    set_all_self = set(cluster for _, cluster in self_clusters.items())
+    set_all_other = set(cluster for _, cluster in other_clusters.items())
+    # create a list which can be used as index for a pandas series
+    df_index_self = list(self_clusters.keys())
+    df_index_other = list(other_clusters.keys())
+    for uuid in df_index_self:
+        files = self_clusters[uuid].set_of_member_ids()
+        rand_index_list = []
+        for uuid_o in df_index_other:
+            files_o = other_clusters[uuid_o].set_of_member_ids()
+            true_positive = len(files & files_o)
+            true_negative = len((set_all_self - files) &
+                                (set_all_other - files_o))
+            false_positive = len(files_o - files)
+            false_negative = len(files - files_o)
+            rand_index = (true_positive + true_negative) / \
+                            (false_positive + false_negative
+                            + true_positive + true_negative)
+            rand_index_list.append(rand_index)
+        series_dict[uuid] = pandas.Series(rand_index_list, df_index_other)
+
+    rand_frame = pandas.DataFrame(series_dict, columns=df_index_self)
+    # to make sure, that not multiple clusters get matched to a cluster of
+    # the comparing set, we perform maxima search once for the original
+    # data frame and once for the transformed one.
+    imax = rand_frame.idxmax()
+    imax_i = rand_frame.T.idxmax()
+    match_dict = {}
+    # first add all clusters which have a match somehow
+    for (column, row) in imax.items():
+        if (rand_frame.at[row, column] > 0) and \
+            ((row, column) in imax_i.items()):
+            match_dict[column] = row
+    # then add all other clusters and mark them by a keyword
+    for uuid in (set(df_index_self) - set(match_dict.keys())):
+        match_dict[uuid] = 'missing'
+    for uuid in (set(df_index_other) - set(match_dict.values())):
+        match_dict[uuid] = 'additional'
+    # generate diffs and some statistics
+    num_tp = 0
+    num_tn = 0
+    num_fp = 0
+    num_fn = 0
+    set_all_files = set()
+    for _, cluster in self_clusters.items():
+        set_all_files |= cluster.cluster_members
+    for (uuid, match_uuid) in match_dict.items():
+        if match_uuid == 'missing':
+    #        print('No match found for cluster', uuid)
+            num_fn += len(self_clusters[uuid].set_of_member_ids())
+
+        elif match_uuid == 'additional':
+    #        print('Generated completely new cluster', uuid)
+            num_fp += len(other_clusters[uuid].set_of_member_ids())
+        else:
+            cluster_match = other_clusters[match_uuid]
+            cluster_gold = self_clusters[uuid]
+            cluster_diff_obj = cluster_gold.compare(cluster_match)
+            num_tp += len(cluster_diff_obj.true_positive)
+            num_tn += len((set_all_files - cluster_gold.cluster_members) &
+                            (set_all_files - cluster_match.cluster_members))
+            num_fp += len(cluster_diff_obj.false_positive)
+            num_fn += len(cluster_diff_obj.false_negative)
+            #print(cluster_diff_obj)
+    #    print('-------------------------------------------------------')
+    #print('tn={}, fn={}, tp={}, fp={}, n={}'.format(
+                        # num_tn, num_fn, num_tp, num_fp,
+                        # len(set_all_files)))
+    #print('Cluster num gold: {}, Cluster num algo: {}.'.format(
+    #        len(df_index_self), len(df_index_other)))
+
+    labels_gold_dict = dict()
+    labels_test_dict = dict()
+    # assign a label to each cluster. Only store the label for scipy's rand
+    # score implementation
+    label = 0
+    for (uuid, match_uuid) in match_dict.items():
+        if match_uuid == 'missing':
+            labels_gold_dict |= { member:label
+                for member in self_clusters[uuid].set_of_member_ids()}
+
+        elif match_uuid == 'additional':
+            labels_test_dict |= { member:label
+                for member in other_clusters[uuid].set_of_member_ids()}
+        else:
+            labels_test_dict |= { member:label
+                for member in other_clusters[match_uuid].set_of_member_ids()}
+            labels_gold_dict |= { member:label
+                for member in self_clusters[uuid].set_of_member_ids()}
+        label += 1
+    
+    labels_gold = []
+    labels_test = []
+
+    # add labels in the same order ot the label vectors
+    for member_id, label in labels_test_dict.items():
+        labels_test.append(label)
+        labels_gold.append(labels_gold_dict[member_id])
+
+    metrics_dict = {
+        'rand' : metrics.rand_score(labels_gold, labels_test),
+        'rand_adjusted' : metrics.adjusted_rand_score(
+            labels_gold, labels_test),
+        'mri' : metrics.normalized_mutual_info_score(labels_gold, labels_test),
+        'accuracy' : metrics.accuracy_score(labels_gold, labels_test),
+        'f1_score' : metrics.f1_score(
+            labels_gold, labels_test, average='macro'),
+    }
+    
+
+    #print('Rand score: ', metrics.rand_score(labels_gold, labels_test))
+    return metrics_dict
---
+++ spamclustering/compare_algorithms.py
@@ -0,0 +1,240 @@
+import os
+import sys
+import threading
+import queue
+import math
+import copy
+import concurrent.futures
+
+#import all algorithms
+import spamclustering.algorithms.cctree as cctree
+import spamclustering.algorithms.clope as clope
+import spamclustering.algorithms.fptree as fptree
+import spamclustering.algorithms.ctph as ctph
+# import all other needed modules from spamclustering
+import spamclustering.benchmarking.clusterdiff as cdiff
+import spamclustering.benchmarking.difftool as dtool
+import spamclustering.mailIo.mailIo as mailIo
+import spamclustering.preprocess.extentedemailmessage as exm
+import spamclustering.preprocess.featureselector as fs
+
+def read_files(input_path):
+    file_list = []
+    out_path = ''
+    if os.path.isdir(input_path):
+        print('Input is directory...')
+        file_list = filter(lambda i: (os.path.splitext(i)[1] == '.eml'),
+                            os.listdir(input_path))
+        file_list = [os.path.join(input_path, file) for file in file_list]
+    else:
+        file_list = [input_path]
+    return file_list
+
+def algorithm_constructor_wrapper(job_id, job_args, features, out_path):
+    algo_name = job_args[0]
+    algo = None
+    match(algo_name):
+        case "CCTree":
+            algo = cctree.CcTreeClustering(features, out_path, job_args[1])
+        case "FPTree":
+            algo = fptree.FPTreeClustering(features, out_path, job_args[1])
+        case "Clope":
+            algo = clope.ClopeClustering(features, out_path, job_args[1])
+        case "CTPH":
+            algo = ctph.Ctph(features, out_path, job_args[1])
+    algo.id = job_id
+    return algo
+
+def create_extended_mail_list(file_list):
+    result = []
+    error_log = []
+    count = 0
+    list_len = len(file_list)
+    illformed_files = set()
+    for f_path in file_list:
+        #print('[{0}|{1}] Processing {2}'.format(count, list_len, file))
+        _, message_id = os.path.split(f_path) 
+        try:
+            message = mailIo.readMailFromEmlFile(f_path)            
+            extMessage = exm.ExtentedEmailMessage(message, f_path)
+            extMessage.extract_payload()
+            result.append(extMessage)
+        except UnicodeEncodeError as u_error:
+            error_string = \
+                'Mail {} is ill formed and therefore skipped!'.format(f_path)
+            error_log += error_string + str(u_error) + '\n'
+            illformed_files.add(message_id)
+            print(error_string)
+        except ValueError as v_error:
+            error_string = \
+                'Mail {} produced a ValueError:{}'.format(fif_pathle,
+                                                          str(v_error))
+            error_log += error_string + '\n'
+            error_log += "Content of mail:\n"
+            error_log += str(extMessage.header_dict) + '\n'
+            illformed_files.add(message_id)
+            print(error_string)
+        count += 1
+    return (result, error_log, illformed_files)
+
+def collect_features(mail_list):
+    featureSelector = fs.FeatureSelector(mail_list)
+    return featureSelector.feature_dict
+
+def perform_clustering(algorithm):
+    algorithm.do_clustering()
+    print("Finished job with ID:", algorithm.id, id(algorithm))
+    return algorithm.id, algorithm.cluster_dict
+
+def parallel_benchmarking(algo_id, cluster1, cluster2):
+    metrics_dict = cdiff.generate_cluster_diff( cluster1, cluster2)
+    return algo_id, metrics_dict
+
+def main():
+    """
+    Performs clustering with the help of a context triggered piecewise hash
+    function. Files found in `<path_to_files>` are processed, similar files
+    are grouped in a cluster and the cluster is then written to
+    `path_to_cluster_out`.
+
+    Run with:
+    
+    python -m spamclustering.example_ctph <path_to_files> 
+    <path_to_cluster_out>
+    
+    from root directory.
+    """
+    argv = sys.argv
+    argc = len(sys.argv)
+    gold_path = ''
+    out_path = ''
+    if argc > 0:
+        file_list = read_files(argv[1])
+        if len(argv) > 2:
+            if os.path.isdir(argv[2]):
+                gold_path = argv[2]
+            else:
+                print("Path to gold standard does not contain a suitable" + 
+                      "clustering. Terminating...")
+                return 
+        if len(argv) > 3:
+            out_path = argv[3]
+            if os.path.isdir(out_path):
+                print('Writing output to', out_path)
+            else:
+                create = input(out_path + ' does not exist. Create? [y|n]')
+                if create == 'y':
+                    os.mkdir(out_path)
+
+        #create objects of class extendedEmailMessage from input files
+        list_of_messages = []
+        error_log = []
+        list_of_messages, error_log, illformed_files = \
+            create_extended_mail_list(file_list)
+
+        num_of_threads = 8
+        #create threads to process files
+        files_to_process = len(list_of_messages)
+        files_per_thread = math.trunc(files_to_process / num_of_threads)
+        thread_list = []
+        thread_files = []
+        offset =  0
+        for x in range(0, num_of_threads-1):
+            thread_files += [
+                list_of_messages[0 + offset:offset + files_per_thread]
+            ]
+            offset += files_per_thread
+        thread_files += [list_of_messages[-(files_to_process - offset):]]
+
+        results = []
+        executor = concurrent.futures.ProcessPoolExecutor(num_of_threads)
+        for files in thread_files:
+            results.append(executor.submit(collect_features, files))
+
+        features = dict()
+        for f in concurrent.futures.as_completed(results):
+             features.update(f.result())
+
+        concurrent.futures.wait(results)
+        results.clear()
+
+        (len(features))
+
+        job_dict = {
+            "CCTree 1" : ["CCTree", 50],
+            "CCTree 2" : ["CCTree", 40],
+            "CCTree 3" : ["CCTree", 30],
+            "CCTree 4" : ["CCTree", 60],
+            "FPTree 1" : ["FPTree", 5],
+            "Clope 1" : ["Clope", 5],
+            "Clope 2" : ["Clope", 3],
+            "Clope 3" : ["Clope", 7.5],
+            "Clope 3" : ["Clope", 10],
+            "CTPH 1" : ["CTPH", 80],
+            "CTPH 2" : ["CTPH", 70],
+            "CTPH 3" : ["CTPH", 60],
+            "CTPH 4" : ["CTPH", 90],
+        }
+
+        num_of_algos = len(job_dict)
+
+        # create for each algorithm and for each configuration of an algorithm
+        # a job to run concurrently
+        algo_list = []
+        for job_id, job_args in job_dict.items():
+            algo_list.append(algorithm_constructor_wrapper(job_id,
+                                                           job_args,
+                                                           features, 
+                                                           out_path))
+
+        for algo in algo_list:
+            results.append(executor.submit(perform_clustering, algo))
+
+        tree = dtool.Tree(gold_path)
+        start = tree.create_from_path(gold_path)
+        gold_clustering = start.to_cluster_set()
+
+        for cluster in gold_clustering:
+            cluster.remove(illformed_files)
+        
+        count = 0
+        for cluster in gold_clustering:
+            for f in cluster.cluster_members:
+                count += 1
+        clusterings = dict()
+
+        for f in concurrent.futures.as_completed(results):
+            algo_id, clustering = f.result()
+            clusterings[algo_id] = clustering
+         
+        concurrent.futures.wait(results)
+
+        for algo in algo_list:
+            algo.cluster_dict = clusterings[algo.id]
+        
+        jobs = []
+        executor = concurrent.futures.ProcessPoolExecutor(num_of_threads)
+        for algo_id, clustering in clusterings.items():
+            other_cluster = set(cluster for _, cluster in clustering.items())
+            jobs.append(executor.submit(
+                    parallel_benchmarking, 
+                    algo_id,
+                    gold_clustering,
+                    other_cluster)
+            )
+        
+        executor.shutdown(wait=True)
+        for j in concurrent.futures.as_completed(jobs):
+            print(j.result())
+        
+        # write clusters to disk if we have an output path
+        if os.path.isdir(out_path):
+            for algo in algo_list:
+                algo_out_path = os.path.join(algo.output_path, algo.id)
+                algo.output_path = algo_out_path
+                if not os.path.isdir(algo_out_path):
+                    os.mkdir(algo_out_path)
+                algo.write_cluster_to_disk()
+
+if __name__ == "__main__":
+    main()
